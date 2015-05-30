673567a3b24c07bd79609770cad8055781aa6da0
Titus von der Malsburg <malsburg@posteo.de>
2015-05-30 13:23:23 -0700

    Adapt R-function to new data format.

--- analysis_scripts/calculate_wmscores.R
+++ analysis_scripts/calculate_wmscores.R
@@ -4,26 +4,26 @@
 
 wm.scores <- function(filename, threshold=2/3) {
 
-  d <- read.table(filename, header=TRUE)
+  d <- read.table(filename, header=T, sep="\t")
   d <- d[d$phase=="test",,drop=F]
 
-  accuracy <- sum(d$verification) / sum(d$level)
+  accuracy <- sum(d$correctly.verified) / sum(d$num.items)
   message(paste("Overall accuracy in the processing task:",
                 format(accuracy, digits=2)))
 
-  pcu <- mean(d$memory / d$level)
-  pcl <- sum(d$memory) / sum(d$level)
-  anu <- sum(d$level == d$memory) / nrow(d) 
-  anl <- sum(d$level * (d$level == d$memory)) / sum(d$level)
+  pcu <- mean(d$correctly.recalled / d$num.items)
+  pcl <- sum(d$correctly.recalled) / sum(d$num.items)
+  anu <- sum(d$num.items == d$correctly.recalled) / nrow(d)
+  anl <- sum(d$num.items * (d$num.items == d$correctly.recalled)) / sum(d$num.items)
 
   thresh.func <- function(x) { mean(x==1)>threshold }
-  t <- tapply(d$memory / d$level, d$level, thresh.func)
+  t <- tapply(d$correctly.recalled / d$num.items, d$num.items, thresh.func)
   wmc <- max(as.integer(c("0", names(t)[t])))
 
-  t <- tapply(d$memory==d$level, d$level,
+  t <- tapply(d$correctly.recalled==d$num.items, d$num.items,
               function(x) { sum(x)/length(x)>threshold })
   wmc <- max(as.integer(c("0", names(t)[t])))
 
-  list(wmc=wmc, pcu=pcu, anu=anu, pcl=pcl, anl=anl, accuracy=accuracy)
+  c(wmc=wmc, pcu=pcu, anu=anu, pcl=pcl, anl=anl, accuracy=accuracy)
 }
 
