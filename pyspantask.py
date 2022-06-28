83cd3d2992934a8da5795878f919fbd62dbc3766
Titus von der Malsburg <malsburg@posteo.de>
2022-06-28 12:20:07 +0200

    Fix focus issue

--- pyspantask.py
+++ pyspantask.py
@@ -613,6 +613,6 @@ if __name__=="__main__":
   w, h = root.winfo_screenwidth(), root.winfo_screenheight()
   #root.overrideredirect(1)
   root.geometry("%dx%d+0+0" % (w, h))
-  root.focus_set()                        # <-- move focus to this widget
+  # root.focus_set()                        # <-- move focus to this widget
   root.bind("<Escape>", lambda e: e.widget.quit())
   root.mainloop()
